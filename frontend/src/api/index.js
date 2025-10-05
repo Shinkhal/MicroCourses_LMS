1ce342fcdff0b92a312fc2bbafc4df96f28091b5
Shinkhal Sinha <shinkhalsinha@gmail.com>
2025-10-05 23:07:39 +0530

    bug fixes

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 // Axios instance
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
